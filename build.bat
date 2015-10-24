fc6fec8fcd57db65436ead456ee4e6da13857e5b
Johnny Shields <johnny.shields@gmail.com>
2015-10-25 02:30:48 +0900

    Add build process via command line

---
+++ build.bat
@@ -0,0 +1,13 @@
+call marmot -c marmot.json fonts/vesper-icons.otf
+
+call "C:\Program Files\7-Zip\7z.exe" e -ofonts webfontkit*.zip vesper-icons.eot vesper-icons.svg vesper-icons.ttf vesper-icons.woff vesper-icons.woff2
+
+rm fonts/vesper-icons-webfont*
+
+mv fonts/vesper-icons.eot fonts/vesper-icons-webfont.eot
+mv fonts/vesper-icons.svg fonts/vesper-icons-webfont.svg
+mv fonts/vesper-icons.ttf fonts/vesper-icons-webfont.ttf
+mv fonts/vesper-icons.woff fonts/vesper-icons-webfont.woff
+mv fonts/vesper-icons.woff2 fonts/vesper-icons-webfont.woff2
+
+rm webfontkit*.zip
